fede0c4fcd5a0b43098f0fa12daaf5ef2d42696d
pablozamit <pablo@pablozamit.com>
2025-05-11 12:18:52 +0000

    v5.11

--- test-firebase.ts
+++ test-firebase.ts
@@ -13,7 +13,11 @@ async function testFirebaseConnection() {
 
     if (!admin.apps.length) {
       admin.initializeApp({
-        credential: admin.credential.applicationDefault(),
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_PROJECT_ID!,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        }),
         databaseURL: dbURL,
       });
     }
