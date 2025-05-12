79f5f738b1d0f2bf6dbb7d4d8f303785d5e038a4
Pablo Zamit <pablo@pablozamit.com>
2025-05-12 06:12:09 +0000

    v5.25

--- src/app/api/searchSpotify/route.ts
+++ src/app/api/searchSpotify/route.ts
@@ -110,6 +110,9 @@ export async function GET(request: Request) {
       console.warn('⚠️ tracks no es un array válido');
       return NextResponse.json({ results: [] });
     }
+    
+    return NextResponse.json({ results }); // ← Esto faltaba
+    
 
     return NextResponse.json({ results });
 
